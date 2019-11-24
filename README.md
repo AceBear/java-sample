a9c26f5c1d4bb1b7bb851a0351e960d13c841789
acebear <vast_m_bear@yahoo.com>
2019-11-24 14:22:48 +0800

    kotlin gradle

---
+++ README.md
@@ -0,0 +1,12 @@
+# java-sample
+
+java的gradle模版 
+
++ gradle基本编译过程
++ aliyun的maven镜像配置(复制gradle.properties开启)
++ log4j2日志
++ junit4单元测试
++ git版本标记
+
+## License
+MIT
---
+++ src/main/java/com/acebear/sample/App.java
@@ -0,0 +1,29 @@
+package com.acebear.sample;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.LoggerContext;
+import org.apache.logging.log4j.core.config.Configuration;
+
+
+public class App {
+    private final static Logger s_logger = LoggerFactory.getLogger(App.class);
+
+    public static void main(String[] args) {
+        s_logger.info("appver: {}", GitVer.getVersion());
+
+        final LoggerContext ctx = (LoggerContext) LogManager.getContext(false);
+        final Configuration config = ctx.getConfiguration();
+        s_logger.info("Active log4j config file: {}", config.getName());
+
+        App app = new App();
+        int a = 3, b=5;
+        s_logger.info("{} + {} = {}", a, b, app.sum(a, b));
+    }
+
+    public int sum(int a, int b){
+        return a+b;
+    }
+}
